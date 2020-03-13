3b016a864a79d588a8984fd2f87b03fa6a7b9f00
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-13 00:35:26 -0300

    Adicionado a pasta Build
    A pasta build possui uma build com a versão minificada do app.

---
+++ build/precache-manifest.15d181430abb15fccab57405f70a06cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2434e57785c60e177980f498a067f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43aff42aa175f0f31019",
+    "url": "/static/js/2.fdad907e.chunk.js"
+  },
+  {
+    "revision": "1e7e1359dfe73ad71c55926bb5998a24",
+    "url": "/static/js/2.fdad907e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc79c7f3567392a8ccae",
+    "url": "/static/js/main.57f8b3c2.chunk.js"
+  },
+  {
+    "revision": "0bbafc4e1ff00e6e0467",
+    "url": "/static/js/runtime-main.a438aab2.js"
+  },
+  {
+    "revision": "bc1790046b9637a8e092202a6b2fe8fb",
+    "url": "/static/media/edit-contact-gif.bc179004.gif"
+  }
+]);
